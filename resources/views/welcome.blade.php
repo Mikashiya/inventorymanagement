f6fbbf69308edfdee9d58020d1797562526ad327
Mikashiya <lemonovamikashiya@gmail.com>
2026-02-17 16:06:39 +0700

    modified:   resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,16 +7,32 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="antialiased bg-neutral-100 flex">
+<body class="antialiased flex">
     <!-- Sidebar -->
-    <div id="sidebar" class="w-[10vw] h-[100vh] shadow-md transition-all">
+    <div id="sidebar" class="w-[10vw] h-[100vh] bg-neutral-100 shadow-md transition-all">
 
     </div>
     <!-- Main Body -->
     <div class="w-full h-[100vh]">
         <!-- Header -->
-        <div class="w-full h-[5vh] shadow-md flex items-center gap-3 px-3">
+        <div class="w-full h-[5vh] bg-neutral-100 shadow-md flex items-center gap-3 px-3">
             <button onclick="toggleSidebar()" class="text-xl">=</button>
+        </div>
+        <!-- Dashboard -->
+        <div class="w-full h-[95vh] p-10 grid gap-10 bg-neutral-800">
+            <div class="grid grid-cols-3 gap-10">
+                <div class="col-span-1 bg-neutral-100 rounded-xl p-5">
+                    <p>TEST</p>
+                </div>
+                <div class="col-span-1 bg-neutral-100 rounded-xl">
+
+                </div>
+            </div>
+            <div class="grid grid-cols-3 gap-10">
+                <div class="col-span-3 bg-neutral-100 rounded-xl">
+
+                </div>
+            </div>
         </div>
     </div>
 </body>
